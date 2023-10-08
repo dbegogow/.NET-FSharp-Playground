81523706f787bfcb14846ae4c72bc07aca3cb197
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-08 10:43:58 +0300

    Partial application

--- FSharpBasics/FSharpBasics/Demo1.fsx
+++ FSharpBasics/FSharpBasics/Demo1.fsx
@@ -7,7 +7,7 @@ let letterA = 'a'
 let isEnable = true
 
 // int -> (int -> int)
-let add x y = x + y
+let add (x: int) (y:int) = x + y
 
 let add' = fun x y -> x + y
 
@@ -16,3 +16,12 @@ let add'' x = fun y -> x + y
 let add5 x =
     let five = 5
     x + five
+
+// let add5' x = add 5 x
+let add5' = add 5
+
+let result = add5' 6
+
+// (2 * (number + 3)) ^ 2
+
+let operation number = (2. * (number + 3.)) ** 2.

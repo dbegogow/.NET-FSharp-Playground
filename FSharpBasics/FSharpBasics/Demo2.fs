1623084d62ae922e41997b3daf779e74bf02f43e
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-11 08:12:41 +0300

    Files, namespaces and modules

---
+++ FSharpBasics/FSharpBasics/Demo2.fs
@@ -0,0 +1,11 @@
+﻿namespace FSharpBasics
+
+module Arithmetic =
+    module Addition = 
+        let add x y = x + y
+
+module Other =
+    open Arithmetic
+
+    let program =
+        Addition.add 5 3

60e88448472cfd67436ed093a007cc496e4afe90
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-15 09:36:09 +0300

    Record type

---
+++ FSharpBasics/FSharpBasics/Demo4.fsx
@@ -0,0 +1,29 @@
+﻿(*
+  Product types:
+    - Record
+    - Typle
+    - Anonymous record
+*)
+
+type Day = { DayOfTheMonth: int; Month: int }
+type Person = { Name: string; Age: int }
+//type Person2 = { Name: string; Age: int }
+
+let dzhulio = { Name = "Dzhulio"; Age = 21 }
+let dzhulio2 = { Name = "Dzhulio"; Age = 20 }
+
+let dzhulioName = dzhulio.Name
+let dzhulioAge = dzhulio.Age
+
+let areEqual = dzhulio = dzhulio2
+let isDzhulioBigger = dzhulio > dzhulio2
+
+let incrementAge person =
+    { person with Age = person.Age + 1 }
+
+let olderDzhulio = incrementAge dzhulio
+
+type Duo = { Person1: Person; Person2: Person }
+
+let alex = { Name = "Alex"; Age = 28 }
+let brothers = { Person1 = dzhulio; Person2 = alex }

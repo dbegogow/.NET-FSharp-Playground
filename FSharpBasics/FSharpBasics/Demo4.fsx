9b60ea4e18d287c0d95b291bf79cc6f50b7e0a26
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-29 10:35:59 +0200

    The 'function' keyword

--- FSharpBasics/FSharpBasics/Demo4.fsx
+++ FSharpBasics/FSharpBasics/Demo4.fsx
@@ -46,14 +46,6 @@ type Suit =
     | Spades
     | Diamonds
 
-type Rectangle = { Base: double; Height: double }
-
-type Shape =
-    | Rectangle of Rectangle
-    | Triangle of height: double * _base: double
-    | Circle of radius: double
-    | Dot
-
 //let yesOrNo bool =
 //    if bool
 //        then "Yes"
@@ -64,6 +56,71 @@ let yesOrNo bool =
     | true -> "Yes"
     | false -> "No"
 
+let yesOrNo' = function
+    | true -> "Yes"
+    | false -> "No"
+
+// Never do this!
+let yesOrNo'' bool = 
+    (function
+    | true -> "Yes"
+    | false -> "No") bool
+
+let isEven = function
+    | x when x % 2 = 0 -> true
+    | _ -> false
+
+let isEven' x = x % 2 = 0
+
+let isOne = function
+    | 1 -> true
+    | _ -> false
+
+let isOne' number = 
+    number = 1
+
+let isOne'' = 
+    (=) 1
+
+let translateFizzBuzz = function
+    | "Fizz" -> string 3
+    | "Buzz" -> string 5
+    | "FizzBuzz" -> string 15
+    | x -> x
+
+let fizzStringNum = translateFizzBuzz "Fizz"
+let buzzStringNum = translateFizzBuzz "Buzz"
+let fizzBuzzStringNum = translateFizzBuzz "FizzBuzz"
+let TomatoStringNum = translateFizzBuzz "Tomato"
+
+type NormalRectangle = { Base: double; Height: double }
+
+type Rectangle =   
+    | Normal of NormalRectangle
+    | Square of side:double
+
+module Rectangle =
+    let area = function
+        | Normal { Base = b; Height = h } -> b * h
+        | Square s -> s ** 2.
+
+type Shape =
+    | Rectangle of Rectangle
+    | Triangle of height: double * _base: double
+    | Circle of radius: double
+    | Dot
+
+let circle = Circle 1.
+let triangle = Triangle (2.,4.)
+
+
 module Shape =
     let area shape =
-        exn
+        match shape with
+        | Rectangle rect -> Rectangle.area rect
+        | Triangle (h, b) -> h * b / 2.
+        | Circle r -> r ** 2. * System.Math.PI
+        | Dot -> 1.
+
+Shape.area circle
+Shape.area triangle
